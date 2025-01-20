7189c1e044f3511b047fc82ca6c9162a705d75a7
Amit Kumar Sutradhar <dev.amitkumarsutradhar@gmail.com>
2025-01-20 13:50:50 +0600

    Add article functionality

--- src/Pages/Article/AddArticle.jsx
+++ src/Pages/Article/AddArticle.jsx
@@ -1,55 +1,109 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import Select from 'react-select';
+import useAxiosPublic from '../../hooks/useAxiosPublic';
+import Swal from 'sweetalert2';
+import { useQuery } from '@tanstack/react-query';
+
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
+
+const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+];
 
 const AddArticle = () => {
+    const [selectedOption, setSelectedOption] = useState([]);
+    const axiosPublic = useAxiosPublic();
+    const { register, handleSubmit, setValue, reset, formState: { errors } } = useForm();
 
-    const { register, handleSubmit, reset } = useForm();
+    const { data: publishers = [], refetch } = useQuery({
+        queryKey: ['publishers'],
+        queryFn: async () => {
+            const res = await axiosPublic.get('/publishers');
+            return res.data;
+        }
+    })
 
-    const options = [
-        { value: 'chocolate', label: 'Chocolate' },
-        { value: 'strawberry', label: 'Strawberry' },
-        { value: 'vanilla', label: 'Vanilla' },
-    ];
+    const handleChange = (selected) => {
+        setSelectedOption(selected);
+        setValue('tags', selected); 
+    };
 
-    // const state = {
-    //     selectedOption: null,
-    // };
 
-    // handleChange = (selectedOption) => {
-    //     setState({ selectedOption }, () =>
-    //         console.log(`Option selected:`, this.state.selectedOption)
-    //     );
-    // };
+    const handleAddArticle = async (data) => {
+        // console.log(data)
 
-    // const { selectedOption } = state;
+        const imageFile = { image: data.image[0] }
+        const res = await axiosPublic.post(image_hosting_api, imageFile, {
+            headers: {
+                'content-type': 'multipart/form-data'
+            }
+        });
+
+        if (res.data.success) {
+            const articleInfo = {
+                title: data.title,
+                publisher: data.publisher,
+                tags: data.tags,
+                description: data.description,
+                image: res.data.data.display_url
+            }
+
+            const articleRes = await axiosPublic.post('/atricles', articleInfo);
+            console.log(articleRes.data)
+
+            if (articleRes.data.insertedId) {
+                reset();
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: `${data.title} is added to the articles.`,
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            }
+        }
+    };
 
 
     return (
         <div className="card bg-base-100 w-1/2 mx-auto my-10 shrink-0 shadow-2xl">
-            <form className="card-body">
+            <form onSubmit={handleSubmit(handleAddArticle)} className="card-body">
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text">Title</span>
                     </label>
                     <input type="text" {...register('title', { required: true })} placeholder="Title" className="input input-bordered" required />
+                    {errors.title?.type === "required" && (
+                            <p role="alert">Title is required</p>
+                        )}
                 </div>
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text">Image</span>
                     </label>
                     <input type="file" {...register('image', { required: true })} className="file-input w-full input-bordered" />
+                    {errors.image?.type === "required" && (
+                            <p role="alert">Image is required</p>
+                        )}
                 </div>
 
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text">Publisher</span>
                     </label>
-                    <select className="select select-bordered w-full max-w-xs">
-                        <option disabled selected>Who shot first?</option>
-                        <option>Han Solo</option>
-                        <option>Greedo</option>
+                    <select defaultValue="default" {...register('publisher', { required: true })} name='publisher' className="select select-bordered w-full">
+                        <option value="default" disabled>Select Option</option>
+                        {
+                            publishers.map(item => <option key={item._id} value={item._id}>{item.name}</option>)
+                        }
                     </select>
+                    {errors.category?.type === "required" && (
+                            <p role="alert">Publisher is required</p>
+                        )}
                 </div>
 
                 <div className="form-control">
@@ -57,20 +111,27 @@ const AddArticle = () => {
                         <span className="label-text">Tags</span>
                     </label>
                     <Select
-                        // value={selectedOption}
-                        // onChange={handleChange}
+                        name='tags'
+                        value={selectedOption}
+                        onChange={handleChange}
+                        isMulti
                         options={options}
                     />
+                      {errors.tags && <p role="alert">Tags is required</p>}
                 </div>
 
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text">Description</span>
                     </label>
-                    <textarea {...register('description', { required: true })} name="" id=""></textarea>
+                    <textarea {...register('description', { required: true })} name="description" id=""></textarea>
+                    {errors.description?.type === "required" && (
+                            <p role="alert">Tags is required</p>
+                        )}
                 </div>
+                
                 <div className="form-control mt-6">
-                    <button className="btn btn-primary">Submit</button>
+                    <button className="btn btn-primary">Add Article</button>
                 </div>
             </form>
         </div>
--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,5 +1,7 @@
 import { useQuery, useQueryClient } from '@tanstack/react-query'
 import useAxiosPublic from '../../../hooks/useAxiosPublic';
+import { FaUsers } from 'react-icons/fa';
+import Swal from 'sweetalert2';
 
 const AllUsers = () => {
     const axiosPublic = useAxiosPublic();
@@ -12,6 +14,50 @@ const AllUsers = () => {
             return res.data;
         }
     })
+
+    const handleMakeAdmin = user =>{
+        axiosPublic.patch(`/users/admin/${user._id}`)
+        .then(res =>{
+            // console.log(res.data)
+            if(res.data.modifiedCount > 0){
+                refetch();
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: `${user.name} is an Admin Now!`,
+                    showConfirmButton: false,
+                    timer: 1500
+                  });
+            }
+        })
+    }
+
+    const handleDeleteUser = user => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                axiosSecure.delete(`/users/${user._id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            refetch();
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
+    }
 
     return (
         <div>
@@ -38,7 +84,7 @@ const AllUsers = () => {
                     <tbody>
                         {
 
-                            users &&  users.map( user => 
+                            users && users.map(user =>
                                 <tr key={user._id}>
                                     <th>
                                         <label>
@@ -60,12 +106,18 @@ const AllUsers = () => {
                                         </div>
                                     </td>
                                     <td>
-                                    <div className="font-bold">{user.email}</div>
+                                        <div className="font-bold">{user.email}</div>
                                     </td>
                                     <td>
+
                                         {
-                                            user?.role ? "Admin" :  "Make Admin"
+                                            user?.role === 'admin' ? 'Admin' : <button
+                                                onClick={() => handleMakeAdmin(user)}
+                                                className="btn btn-lg bg-orange-500">
+                                                <FaUsers className="text-white text-2xl"></FaUsers>
+                                            </button>
                                         }
+
                                     </td>
                                     <th>
                                         <button className="btn btn-ghost btn-xs">details</button>
