6b4ccd4cccb44f6a1157260ba907652fe515c49f
Amit Kumar Sutradhar <dev.amitkumarsutradhar@gmail.com>
2025-01-20 18:45:06 +0600

    Firebase deploy setup

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,17 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCkU8NMDeMTE9pQrofDYBdeqXxq6FoqoA4",
-  authDomain: "test-project-1e8c2.firebaseapp.com",
-  projectId: "test-project-1e8c2",
-  storageBucket: "test-project-1e8c2.firebasestorage.app",
-  messagingSenderId: "900261382915",
-  appId: "1:900261382915:web:917ce70e60286fce6af736",
-  measurementId: "G-31TY6Y8CGN"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
+
+
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
