7e1d2dd1e292eb6c4ed491f0896d6c4951984b65
Amit Kumar Sutradhar <dev.amitkumarsutradhar@gmail.com>
2025-01-20 11:42:20 +0600

    Show user on all users page

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import { useQuery, useQueryClient } from '@tanstack/react-query'
+import useAxiosPublic from '../../../hooks/useAxiosPublic';
 
 const AllUsers = () => {
+    const axiosPublic = useAxiosPublic();
+
+    const { data: users = [], refetch } = useQuery({
+        queryKey: ['users'],
+        queryFn: async () => {
+            const res = await axiosPublic.get('/users');
+            // console.log(res);
+            return res.data;
+        }
+    })
+
     return (
         <div>
 
 
             <div className="overflow-x-auto">
                 <h2 className='text-4xl text-left p-3'>All Users</h2>
+
                 <table className="table">
                     {/* head */}
                     <thead>
@@ -17,44 +30,51 @@ const AllUsers = () => {
                                 </label>
                             </th>
                             <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
-                            <th></th>
+                            <th>Email</th>
+                            <th>Role</th>
+                            <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {/* row 1 */}
-                        <tr>
-                            <th>
-                                <label>
-                                    <input type="checkbox" className="checkbox" />
-                                </label>
-                            </th>
-                            <td>
-                                <div className="flex items-center gap-3">
-                                    <div className="avatar">
-                                        <div className="mask mask-squircle h-12 w-12">
-                                            <img
-                                                src="https://img.daisyui.com/images/profile/demo/2@94.webp"
-                                                alt="Avatar Tailwind CSS Component" />
+                        {
+
+                            users &&  users.map( user => 
+                                <tr key={user._id}>
+                                    <th>
+                                        <label>
+                                            <input type="checkbox" className="checkbox" />
+                                        </label>
+                                    </th>
+                                    <td>
+                                        <div className="flex items-center gap-3">
+                                            <div className="avatar">
+                                                <div className="mask mask-squircle h-12 w-12">
+                                                    <img
+                                                        src="https://img.daisyui.com/images/profile/demo/2@94.webp"
+                                                        alt="Avatar Tailwind CSS Component" />
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <div className="font-bold">{user.name}</div>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div>
-                                        <div className="font-bold">Hart Hagerty</div>
-                                        <div className="text-sm opacity-50">United States</div>
-                                    </div>
-                                </div>
-                            </td>
-                            <td>
-                                Zemlak, Daniel and Leannon
-                                <br />
-                                <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
-                            </td>
-                            <td>Purple</td>
-                            <th>
-                                <button className="btn btn-ghost btn-xs">details</button>
-                            </th>
-                        </tr>
+                                    </td>
+                                    <td>
+                                    <div className="font-bold">{user.email}</div>
+                                    </td>
+                                    <td>
+                                        {
+                                            user?.role ? "Admin" :  "Make Admin"
+                                        }
+                                    </td>
+                                    <th>
+                                        <button className="btn btn-ghost btn-xs">details</button>
+                                    </th>
+                                </tr>
+                            )
+
+                        }
+
                     </tbody>
                     {/* foot */}
                     <tfoot>
--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,20 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import Router from './Routes/Routes.jsx';
 import { HelmetProvider } from 'react-helmet-async';
 import AuthProvider from './providers/AuthProvider.jsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-      <HelmetProvider>
-        <BrowserRouter>
-          <Router></Router>
-        </BrowserRouter>
-      </HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <HelmetProvider>
+          <BrowserRouter>
+            <Router></Router>
+          </BrowserRouter>
+        </HelmetProvider>
+      </QueryClientProvider>
     </AuthProvider>
   </StrictMode>,
 )
